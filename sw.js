00d759e73d374a7a286456773ffdb6ad08b56893
Duc Tai TRAN <tdts2010@gmail.com>
2024-05-22 00:07:16 +0200

    test

--- sw.js
+++ sw.js
@@ -2,42 +2,30 @@ const cacheName = "ToDoList"
 const contentToCache = [
   "./",
 	"./index.html",
-	"./style.css",
-	"./app.js",
+	"./index.css",
+	"./index.js",
 	"./sw.js",
 ]
 
 self.addEventListener("install", (e) => {
 	console.log("Service Worker installed");
 	e.waitUntil(
-    caches.open(cacheName)
-			.then(cache =>{return cache.addAll(contentToCache);
-		})
-	)
-})
+		(async () => {
+			const cache = await caches.open(cacheName);
+			await cache.addAll(contentToCache);
+		})()
+	);
+});
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        const fetchRequest = event.request.clone();
-        return fetch(fetchRequest).then(response => {
-          if(!response || response.status !== 200 || response.type !== 'basic') {
-            return response;}
-            var responseToCache = response.clone();
-
-            caches.open(cacheName
-            )
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-          });
-        return response
-      }
-  )
-})
-  )
-})
+self.addEventListener("fetch", function (event) {
+	event.respondWith(fetch(event.request).then((res) => {
+		let response = res.clone();
+        caches.open(cacheName).then((cache) => {
+          cache.put(event.request, response);
+        });
+		return res
+	}).catch((err) => {
+		return caches.match(event.request)
+	})
+	);
+});
