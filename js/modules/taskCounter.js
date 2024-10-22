573b654fe47a88ef1b0fe13f2ecc3901c2c99922
Duc Tai TRAN <tdts2010@gmail.com>
2024-10-22 23:47:54 +0200

    test

---
+++ js/modules/taskCounter.js
@@ -0,0 +1,14 @@
+const todoList = document.getElementById('todolist');
+const totalCountDisplay = document.getElementById('total-count');
+const completedCountDisplay = document.getElementById('completed-count');
+function updateTaskCount() {
+  const totalCount = todoList.children.length;
+  const completedCount = todoList.querySelectorAll('input[type="checkbox"]:checked').length;
+  totalCountDisplay.textContent = `Tasks number : ${totalCount}`;
+  completedCountDisplay.textContent = `Tasks done : ${completedCount}`;
+  console.log("change")
+}
+
+const observer = new MutationObserver(updateTaskCount);
+observer.observe(todoList, { childList: true, subtree: true,attributes: true,
+  attributeFilter: ['class']  });
